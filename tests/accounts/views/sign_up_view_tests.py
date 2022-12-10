f79f26f3636321710faee515b48e3096c22cb949
Velina Genova <viligen@gmail.com>
2022-12-10 10:55:27 +0100

    tests

--- tests/accounts/views/sign_up_view_tests.py
+++ tests/accounts/views/sign_up_view_tests.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+
+UserModel = get_user_model()
 
 
 class SignUpViewTests(TestCase):
@@ -10,10 +13,15 @@ class SignUpViewTests(TestCase):
         'password2': '123.my4567@Q',
     }
 
-    def test_SignUpView__when_valid_data__assert_success_login_id_1(self):
-        response = self.client.post(reverse('register'), data=self.VALID_DATA, )
+    def test_SignUpView__when_valid_data__assert_success__id_exists(self):
+        self.client.post(reverse('register'), data=self.VALID_DATA, )
 
-        self.assertEqual('1', self.client.session['_auth_user_id'])
+        self.assertEqual(int(self.client.session['_auth_user_id']), UserModel.objects
+                         .filter(pk=self.client.session['_auth_user_id']).get().pk)
+
+    def test_SignUpView__when_valid_data__assert_success__users_count__expect_1(self):
+        self.client.post(reverse('register'), data=self.VALID_DATA, )
+        self.assertEqual(1, UserModel.objects.count())
 
     def test_SignUpView__when_valid_data__assert_successful_redirect_302(self):
         response = self.client.post(reverse('register'), data=self.VALID_DATA, )
